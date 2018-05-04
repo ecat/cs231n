b5753403c18fc680d73b4e66479906e5864627bd
Philip Kenneth Lee <pkllee@vigata.stanford.edu>
2018-05-04 11:30:59 -0700

    Fixed generate undersampled dataset to support real and complex data generation

--- matlab/generate_us_dataset.m
+++ matlab/generate_us_dataset.m
@@ -2,6 +2,8 @@
 addPaths()
 data_path = 'data/scan2/kspace';
 
+generate_real_data = true;
+
 ksp = readReconData(data_path);
 [nx, ny, nz, nc] = size(ksp);
 
@@ -9,19 +11,37 @@ ksp = readReconData(data_path);
 %% do undersampling
 Ry = 1;
 Rz = 2;
-calib_size = 10;
+calib_size = round(nz/20); % retain 5 percent of lines
+calib_size = calib_size + mod(calib_size, 2) + 1; % ensure odd acs lines
+
 output_dims = [1 ny nz 1];
 arc_mask = get_arc_mask(ny, nz, Ry, Rz, calib_size, output_dims);
 
-ksp_real = fft3c(abs(ifft3c(ksp)));
-ksp_us = bsxfun(@times, ksp_real, arc_mask);
+if(generate_real_data)
+    display('generating real data')
+    ksp_to_us = fft3c(abs(ifft3c(ksp)));
+else
+    display('generating complex data')
+    ksp_to_us = ksp;
+end
+
+ksp_us = bsxfun(@times, ksp_to_us, arc_mask);
 
 
 %% reformat into axial slices and normalize to be between 0, 1
 im_us = ifft3c(ksp_us);
 im_us = permute(im_us, [3 2 1 4]);
 
-display(['imag to real ratio' num2str(sum(abs(imag(im_us(:)))) / sum(abs(real(im_us(:)))))])
-im_us = im_us / max(abs(im_us(:))); 
+display(['imag to real ratio ' num2str(sum(abs(imag(im_us(:)))) / sum(abs(real(im_us(:)))))])
+
+im_ref = sos(ifft3c(ksp), 4);
+im_ref = permute(im_ref, [3 2 1 4]);
+
+norm_factor = max(abs(im_ref(:)));
+
+im_ref = im_ref / norm_factor; 
+im_us = im_us / norm_factor;
+
+figure; imshow3s([sos(im_us, 4), im_ref])
 
 %% save data
--- matlab/utils/get_us_mask.m
+++ matlab/utils/get_us_mask.m
@@ -1,19 +1,18 @@
 function [ us_mask ] = get_us_mask( ny, nz, Ry, Rz, output_dims  )
 %GET_US_MASK 
-    if(nargin < 4)
+    if(nargin < 5)
         output_dims = -1;
     end
     
-    us_mask = ones(ny, nz);
+    us_mask = zeros(ny, nz);
     
-    if(Ry > 1)
-        us_mask(1:Ry:end, :) = 0;
-    end
-    
-    if(Rz > 1)
-        us_mask(:, 1:Rz:end) = 0;
-    end
-    
+    if(Ry > 1 && Rz == 1)
+        us_mask(1:Ry:end, :) = 1;
+    elseif(Rz > 1 && Ry == 1)
+        us_mask(:, 1:Rz:end) = 1;
+    else
+        us_mask(1:Ry:end, 1:Rz:end) = 1;
+    end    
     
     if(prod(output_dims) == ny * nz) 
         us_mask = reshape(us_mask, [1 ny nz 1]);
