2234b95118f221f91b51d23238092ca9b03a5ad2
Philip Kenneth Lee <pkllee@vigata.stanford.edu>
2018-05-04 11:23:16 -0700

    Fixed calibration mask to be purely real sinc blurring

--- matlab/utils/get_calib_mask.m
+++ matlab/utils/get_calib_mask.m
@@ -2,15 +2,23 @@ function [ calib_mask ] = get_calib_mask( ny, nz, calib_size, output_dims )
 %GET_CALIB_MASK Summary of this function goes here
 %   ny
 %   nz
-% calib_size
+% calib_size, odd number, includes the center region
 % output_dims = -1 or 0 to keep default
     if(nargin < 4)
         output_dims = -1;
     end
         
-
-    calib_mask = zeros(ny, nz); 
-    calib_mask(ny/2 - calib_size/2 + 1: ny/2 + calib_size/2, nz/2 - calib_size/2 + 1: nz/2 + calib_size/2) = 1;
+    
+    % this mask ensures that sinc pattern is real, can verify using code
+    % below
+    % a = [zeros(15, 1); ones(11, 1); zeros(14, 1)];
+    % b = fftc(a, 1);
+    % figure; subplot(211); plot(abs(b)); subplot(212); plot(angle(b));%
+    
+    calib_mask = zeros(ny, nz);     
+    calib_extent = floor(calib_size/2);
+    
+    calib_mask(ny/2 - calib_extent + 1: ny/2 + calib_extent + 1, nz/2 - calib_extent + 1: nz/2 + calib_extent + 1) = 1;
     
     if(prod(output_dims) == ny * nz) 
         calib_mask = reshape(calib_mask, [1 ny nz 1]);
