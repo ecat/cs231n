76a43f0e4fe6c203bceff3a4c8ef787e9e4169ad
makaimann <makaim@stanford.edu>
2018-05-14 16:27:29 -0700

    Let user specify convolution parameters

--- python/unetblocks.py
+++ python/unetblocks.py
@@ -1,21 +1,24 @@
 import tensorflow as tf
 
-F=16
-kernel_size = (3, 3)
-strides = (1, 1)
+def res_block(Conv1, Conv2=None):
+    def _res_block_helper(x, resblocks=[]):
+        if len(resblocks) > 0:
+            resblock = resblocks[0]
+            resblocks = resblocks[1:]
+            c1 = Conv1(x)
+            m1 = tf.keras.layers.MaxPool2D(pool_size=(2, 2), padding='same')(c1)
+            rb1 = resblock(m1, resblocks)
+            up1 = tf.keras.layers.UpSampling2D(size=(2, 2))(rb1)
+            concatenated_input = tf.keras.layers.concatenate([up1, x], 3)
+            y = Conv2(concatenated_input)
+        else:
+            # bottom of the bowl
+            if Conv2 is not None:
+                print("Warning: Not using second convolution at the bottom of the U.")
+            y = Conv1(x)
 
-def res_block(x, resblocks=[]):
-    if len(resblocks) > 0:
-        resblock = resblocks[0]
-        resblocks = resblocks[1:]
-        c1 = tf.keras.layers.Conv2D(filters=F, kernel_size=kernel_size, padding='same', strides=strides, activation='relu')(x)
-        m1 = tf.keras.layers.MaxPool2D(pool_size=(2, 2), padding='same')(c1)
-        rb1 = resblock(m1, resblocks)
-        up1 = tf.keras.layers.UpSampling2D(size=(2, 2))(rb1)
-        concatenated_input = tf.keras.layers.concatenate([up1, x], 3)
-        y = tf.keras.layers.Conv2D(filters=F, kernel_size=kernel_size, padding='same', strides=strides, activation='relu')(concatenated_input)
-    else:
-        # bottom of the bowl
-        y = tf.keras.layers.Conv2D(filters=F, kernel_size=kernel_size, padding='same', strides=strides)(x)
+        return y
+    return _res_block_helper
 
-    return y
+def gen_conv(filters, kernel_size, strides=(1, 1), padding='same', activation='relu', **kwargs):
+    return tf.keras.layers.Conv2D(filters=filters, kernel_size=kernel_size, strides=strides, padding=padding, activation=activation, **kwargs)
