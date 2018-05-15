811c02efbc878b7dba8033210b715d9c3ebedc17
ecat <pkl.lee@stanford.edu>
2018-05-15 01:26:50 -0700

    Got a 'good' result using default parameters. Need learning rate to be relatively small to get error to order of 10e-5

--- python/unetblocks.py
+++ python/unetblocks.py
@@ -3,13 +3,20 @@ import tensorflow as tf
 def res_block(Conv1, Conv2=None):
     def _res_block_helper(x, resblocks=[]):
         if len(resblocks) > 0:
+            # init first block
             resblock = resblocks[0]
             resblocks = resblocks[1:]
             c1 = Conv1(x)
             m1 = tf.keras.layers.MaxPool2D(pool_size=(2, 2), padding='same')(c1)
+            
+            # init outputs of first block
             rb1 = resblock(m1, resblocks)
             up1 = tf.keras.layers.UpSampling2D(size=(2, 2))(rb1)
+            
+            # combine output of first block, incoming resnet outputs
             concatenated_input = tf.keras.layers.concatenate([up1, x], 3)
+            
+            # init second block
             y = Conv2(concatenated_input)
         else:
             # bottom of the bowl
@@ -20,21 +27,36 @@ def res_block(Conv1, Conv2=None):
         return y
     return _res_block_helper
 
-def gen_conv(filters, kernel_size, strides=(1, 1), padding='same', activation='relu', **kwargs):
-    return tf.keras.layers.Conv2D(filters=filters, kernel_size=kernel_size, strides=strides, padding=padding, activation=activation, **kwargs)
+def gen_conv(filters, kernel_size, strides=(1, 1), padding='same', **kwargs):
+    return tf.keras.layers.Conv2D(filters=filters, kernel_size=kernel_size, strides=strides, padding=padding, activation='linear', **kwargs)
+
+def gen_conv_relu(N=1, **kwargs):
+    layers = [
+        gen_conv(**kwargs),
+        tf.keras.layers.Activation('relu')
+    ]
+    
+    def gen_layers(x): 
+        for _ in range(N):
+            for layer in layers:
+                x = layer(x)
+        return x
+    
+    return gen_layers
 
 
-def gen_conv_bn_relu(**kwargs):               
+def gen_conv_bn_relu(N=1, **kwargs):               
     
     layers = [
         gen_conv(**kwargs),
         tf.keras.layers.BatchNormalization(),
         tf.keras.layers.Activation('relu')
-    ]
+    ]    
     
     def gen_layers(x):
-        for layer in layers:
-            x = layer(x)            
+        for _ in range(N):
+            for layer in layers:
+                x = layer(x)            
         return x
     
     return gen_layers
