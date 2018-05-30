e8c14363fe1640bb305807d8ef30a3369b1c7002
makaimann <makaim@stanford.edu>
2018-05-30 11:37:10 -0700

    Add layernorm implementation

---
+++ python/layers.py
@@ -0,0 +1,28 @@
+import tensorflow as tf
+
+# Credit to JulesGM -- https://github.com/keras-team/keras/issues/3878
+class LayerNorm1D(tf.keras.layers.Layer):
+    def __init__(self, eps=1e-6, **kwargs):
+        self.eps = eps
+        super(LayerNorm1D, self).__init__(**kwargs)
+
+    def build(self, input_shape):
+        self.gamma = self.add_weight(name='gamma',
+                                     shape=input_shape[1:],
+                                     initializer=tf.keras.initializers.Ones(),
+                                     trainable=True)
+
+        self.beta = self.add_weight(name='beta',
+                                    shape=input_shape[1:],
+                                    initializer=tf.keras.initializers.Zeros(),
+                                    trainable=True,)
+
+        super(LayerNorm1D, self).build(input_shape)
+
+    def call(self, x):
+        mean = tf.keras.backend.mean(x, axis=-1, keepdims=True)
+        std = tf.keras.backend.std(x, axis=-1, keepdims=True)
+        return self.gamma * (x - mean) / (std + self.eps) + self.beta
+
+    def compute_output_shape(self, input_shape):
+        return input_shape
