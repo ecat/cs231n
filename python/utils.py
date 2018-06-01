50540a390d691dd0f3ef90fda1b31b102c25f2f4
makaimann <makaim@stanford.edu>
2018-06-01 12:50:44 -0700

    Update conv plotting

--- python/utils.py
+++ python/utils.py
@@ -3,6 +3,9 @@ import matplotlib
 import numpy as np
 import tensorflow as tf
 
+from collections import Sequence
+import math
+
 def gen_interm_fun(inputs, layer):
 
     fun = tf.keras.backend.function(inputs=[inputs], outputs=[layer.output])
@@ -23,28 +26,49 @@ def gen_interm_fun(inputs, layer):
     return _forward_pass
 
 
-def plot_convs(layer, rows, cols):
-    fig, axes = plt.subplots(nrows=rows, ncols=cols)
-    filters = layer.get_weights()
+def plot_convs(layer, channels=None, filters=None):
+    '''
+    Plots heatmaps for kernels
 
-    convs = filters[0]
-    biases = filters[1]
+    channels: An iterable of channels to plot. If None, plots all of them
+    filters: An iterable of filters to plot. If None, plots all of them
+    '''
+    sublayers = layer.get_weights()
+    convs = sublayers[0]
+    biases = sublayers[1]
+
+    if channels == None:
+        channels = range(convs.shape[2])
+    if filters == None:
+        filters = range(convs.shape[-1])
+
+    if not isinstance(channels, Sequence):
+        channels = [channels]
+    if not isinstance(filters, Sequence):
+        filters = [filters]
+
+    cols = 4 # seems like a good number
+    rows = int(math.ceil((len(channels) * len(filters)) / 4.0))
+
+    fig, axes = plt.subplots(nrows=rows, ncols=cols)
+
     idx = 0
 
     cmin = np.amin(convs)
     cmax = np.amax(convs)
 
-    for i in range(rows):
-        for j in range(cols):
+    for i in channels:
+        for j in filters:
             idx += 1
             ax = plt.subplot(rows, cols, idx)
+            ax.set_title('filter={} channel={}'.format(j, i))
             im = ax.imshow(convs[:,:,i,j], cmap=plt.cm.coolwarm, vmin=cmin, vmax=cmax)
             plt.xticks(np.array([]))
             plt.yticks(np.array([]))
 
     plt.tight_layout()
 
-    plt.rcParams['figure.figsize'] = [cols, rows]
+    plt.rcParams['figure.figsize'] = [2*cols, 2*rows]
 
     # Make an axis for the colorbar on the right side
     cax = fig.add_axes([1.2, 0.1, 0.08, 0.8])
